bd89d5f8f7350cd21ccb92eaa7a0ab1a2a3179e9
ryojiroakiyama <ryojiro25@gmail.com>
2023-01-12 16:16:40 +0900

    impl soc

--- contracts/GovernmentExample.sol
+++ contracts/GovernmentExample.sol
@@ -4,6 +4,49 @@ pragma solidity ^0.8.17;
 
 import "./interfaces/IGovernment.sol";
 
-// contract Government is IGovernment {
+contract Government is IGovernment {
+    address private _candidate;
+    address private _voter;
+    address private _president;
+    uint256 private _numOfVotes;
 
-// }
+    function runForCandidate() external {
+        _candidate = msg.sender;
+    }
+
+    function isCandidate(address _verifyAddress) external view returns (bool) {
+        if (_candidate == _verifyAddress) {
+            return true;
+        }
+        return false;
+    }
+
+    function numberOfVotes(address _candidateAddress)
+        external
+        view
+        returns (uint256)
+    {
+        if (_candidate == _candidateAddress) {
+            return _numOfVotes;
+        }
+        return 0;
+    }
+
+    function vote(address _candidateAddress) external {
+        _voter = _candidateAddress;
+    }
+
+    function abstain() external {
+        _voter = msg.sender;
+    }
+
+    function determinePresident() external {
+        _president = _candidate;
+    }
+
+    function getPresident() external view returns (address) {
+        return _president;
+    }
+
+    // More functions
+}
